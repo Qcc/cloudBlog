0d2066d6b774bb61591d1c38bcd64c5eb3a774fd
kevin <kevin.e@qq.com>
2017-11-09 17:25:06 +0800

    re

--- README.md
+++
@@ -1,3 +0,0 @@
-
-# cloudBlog
-cloudBlog
---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.use('/dir',express.static(__dirname + 'public',{
+  redirect:true
+}));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+// 404 page
+// app.use(function (req, res) {
+//   if (!res.headersSent) {
+//     res.end('error,not found page!');
+//   }
+// });
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
