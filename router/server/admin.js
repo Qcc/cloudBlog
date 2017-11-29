64fd80e4bc1dd446080c0b172d1fba99dcc1fc38
kevin <kevin.e@qq.com>
2017-11-29 16:05:08 +0800

    views

--- router/server/admin.js
+++ router/server/admin.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/login',function(req,res,next){
-  res.header("X-Powered-By", 'ASP.NET');
-  res.render('./server/login',{title:'个人博客'});
+router.get('/overview.html',function(req,res,next){
+  res.render('./server/overview');
+});
+router.get('/section.html',function(req,res,next){
+  res.render('./server/section');
+});
+router.get('/pusharticle.html',function(req,res,next){
+  res.render('./server/pusharticle');
+});
+router.get('/articlemanage.html',function(req,res,next){
+  res.render('./server/articlemanage');
+});
+router.get('/pushpicture.html',function(req,res,next){
+  res.render('./server/pushpicture');
+});
+router.get('/picturemanage.html',function(req,res,next){
+  res.render('./server/picturemanage');
 });
 
 
